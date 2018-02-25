8aa072fe550079ea2531abfbbdd15996c538d389
oovwall <oovwall@163.com>
2018-02-25 15:20:33 +0800

    更新

--- run.py
+++ run.py
@@ -2,31 +2,42 @@ from flask import Flask, render_template, request, Response, stream_with_context
 
 app = Flask(__name__)
 
+
 # 包含页面
 # #左侧导航菜单
 @app.route('/include-nav.html')
 def includeNav():
     return render_template('include-nav.html')
 
+
 # #底部版权信息
 @app.route('/include-footer.html')
 def includeFooter():
     return render_template('include-footer.html')
+
 
 # 主页
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 # 登录页
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 # 接口列表页
 @app.route('/interface-list')
 def interfaceList():
     return render_template('interface-list.html')
+
+
+# 接口列表页
+@app.route('/interface')
+def interface():
+    return render_template('interface.html')
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,19 @@
-/**
- * Load 回调函数
- */
-function loadBack() {
-  $("#side-menu").metisMenu()
-}
-
 $(function () {
-  try {
-    $('.navbar-default').load('include-nav.html', loadBack)
-    $('.footer').load('include-footer.html')
-  } catch (e) {
-
-  }
-
   //点击删除弹出提示框时确认操作
-  $(document).on("click",".popover-content .label-warning",function(){
-      $(this).parentsUntil("tbody").slideUp(function(){
-          $(this).remove();
-      });
-  });
+  $(document).on('click', '.popover-content .label-warning', function () {
+    $(this).parentsUntil('tbody').slideUp(function () {
+      $(this).remove()
+    })
+  })
 
   //点击删除弹出提示框时取消操作
-  $(document).on("click",".popover-content .label-info",function(){
-      $(this).parentsUntil("td").popover("hide");
-  });
+  $(document).on('click', '.popover-content .label-info', function () {
+    $(this).parentsUntil('td').popover('hide')
+  })
 
   $('[data-toggle="popover"]').popover({
-      selector: '.fa-trash-o',
-      html: true,
-      content: '<span class="label label-warning"><i class="fa fa-check"></i> 是</span> <span class="label label-info"><i class="fa fa-times"></i> 否</span>'
-  });
+    selector: '.fa-trash-o',
+    html: true,
+    content: '<span class="label label-warning"><i class="fa fa-check"></i> 是</span> <span class="label label-info"><i class="fa fa-times"></i> 否</span>'
+  })
 })
