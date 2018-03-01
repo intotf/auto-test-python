13a73a374e7baa16764633a7b177cec3db706ed7
oovwall <oovwall@163.com>
2018-03-01 09:33:44 +0800

    更新

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
+/**
+ * 获取URL中的参数
+ * @param name 参数名
+ */
+function getUrlParam (name) {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)') //构造一个含有目标参数的正则表达式对象
+  var r = window.location.search.substr(1).match(reg)  //匹配目标参数
+  if (r != null) return unescape(r[2])
+  return null //返回参数值
+}
+
 $(function () {
   /*
   //点击删除弹出提示框时确认操作
